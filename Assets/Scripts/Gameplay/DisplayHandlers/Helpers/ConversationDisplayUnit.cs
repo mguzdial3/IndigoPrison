a5501016233e38b4b0c8f466939e8a2e9ea46964
mguzdial3 <mguzdial@gmail.com>
2014-09-15 23:45:21 -0400

    Initial Commit
    All the relevant gameplay bits are there and functioning (on Android at
    least), so all we have to focus on for a bit is the A.I. Check out
    GameplayManager.cs for TODOs over the relevant lines.

---
+++ Assets/Scripts/Gameplay/DisplayHandlers/Helpers/ConversationDisplayUnit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationDisplayUnit : MonoBehaviour {
+	public GUITexture background;
+	public GUIText characterName;
+	public GUIText dialogueLine;
+
+	public delegate void ReadInConversationDisplay(ConversationDisplayUnit unit);
+	private ReadInConversationDisplay m_readConversationDisplay; 
+
+	//Max number of characters to show (otherwise truncate to ...)
+	private const int MAX_CHARACTERS = 25;
+	private const string TRUNCATED_END="...";
+
+	public void SetUpDialogue(string character, string dialogue, Color colorOfCharacter, Color colorOfDialogue){
+		characterName.text = character;
+		characterName.color = colorOfCharacter;
+
+		if (!string.IsNullOrEmpty (dialogue) && dialogue.Length > MAX_CHARACTERS) {
+			dialogue = dialogue.Substring(0,MAX_CHARACTERS);
+			dialogueLine.text = dialogue+TRUNCATED_END;
+		}
+		else{
+			dialogueLine.text = dialogue;
+		}
+
+		dialogueLine.color = colorOfDialogue;
+	}
+
+	public bool IsSameConversation(string character){
+		return !string.IsNullOrEmpty (characterName.text) && !string.IsNullOrEmpty (character) && character.Equals (characterName.text);
+	}
+
+	public void SetReadInConversation(ReadInConversationDisplay readConversation){
+		m_readConversationDisplay = readConversation;
+	}
+
+	public void ButtonClick(UIButton buttonClicked){
+		m_readConversationDisplay (this);
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Handles
+public class GameplayManager : MonoBehaviour {
+	private Dictionary<string, DisplayHandler> m_displays; //Hold all the displays for ease of access
+	public string displayToStartWith;// For Testing
+
+	private string m_currDisplay;
+
+	void Awake () {
+		//Add all the necessary components
+		LocationHandler locationHandler = gameObject.AddComponent<LocationHandler> ();
+		locationHandler.Init ();
+
+		//TODO; Instantiate the situation, characters and their initial goals
+
+
+		//Set up displays
+		m_displays = new Dictionary<string,DisplayHandler >();
+		DisplayHandler[] displayChildren = gameObject.GetComponentsInChildren<DisplayHandler>();
+
+		foreach (DisplayHandler display in displayChildren) {
+			display.Init();
+			display.HideDisplay();
+			m_displays.Add(display.DisplayName,display);
+		}
+
+		//Start the display to start with
+		if (m_displays.ContainsKey (displayToStartWith)) {
+			m_currDisplay = displayToStartWith;
+			m_displays[m_currDisplay].SwitchToDisplay();
+		}
+		else{
+			Debug.LogError("GameplayManager Error: displayToStartWith did not match a Handler name"); 
+		}
+	}
+
+		
+	// Update is called once per frame
+	void Update () {
+		//Handles the Display
+		if(m_displays.ContainsKey(m_currDisplay)){
+			string nextDisplay = m_displays [m_currDisplay].UpdateDisplay ();
+
+			if (!string.IsNullOrEmpty (nextDisplay) && !nextDisplay.Equals (m_currDisplay) && m_displays.ContainsKey(nextDisplay)) {
+				SwitchDisplays(nextDisplay);
+			}
+		}
+
+		//TODO; Determine what state we are in (Player Moving to Goal, Player Chatting, A.I. Replanning)
+
+		//TODO; Implement waiting/timer for when player is moving to goal
+
+		//TODO; Implement chatbot whatever
+
+		//TODO; Implement A.I. Replanning and all content for plans
+	}
+
+	private void SwitchDisplays(string nextDisplay){
+		//TODO; Might want to add a nice fade in/out effect later
+		m_displays [m_currDisplay].SwitchFromDisplay ();
+
+		m_currDisplay = nextDisplay;
+		m_displays [m_currDisplay].SwitchToDisplay ();
+	}
+
+
+
+}
