7680cccfe86c30cf66c3efb64519f19684d90841
kasiu <kasiu@gatech.edu>
2014-10-06 14:51:28 -0400

    Removed top-level old files. Added gitignore/library.

---
+++ external/Indigo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indigo
+{
+    public class Class1
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello Indigo Prison!");
+        }
+    }
+}
