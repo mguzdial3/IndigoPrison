4057b3524c7d9647dcc4279ba35239d4cd6c9a97
Tory S. Anderson <torys.anderson@gmail.com>
2014-10-21 18:14:27 -0400

    Character class created

--- external/Indigo/Character.cs
+++ external/Indigo/Character.cs
@@ -5,21 +5,37 @@ using System.Text;
 
 namespace Indigo
 {
-    /// <summary>
-    /// A dummy placeholder for characters. Replace with Tory's implementation.
-    /// (Although please consider keeping them a struct or else changes will need to be made to everything else.)
-    /// </summary>
+
     public struct Character
     {
-        // Dummy constructor.
+
         public Character(string name) : this()
         {
             this.Name = name;
             this.Alive = true;
+			this.Relationships = new Dictionary<Character, feelingsAboutChar>();
+			//this.nextAction = new Action();
+			this.Goal = new List<Condition> ();
         }
 
         // Dummy properties.
         public string Name { get; private set; }
         public bool Alive { get; set; }
+	    //public List<Attribute> attributes; // TODO emotions, trust, etc
+	    public List<Condition> Goal {get; set;} 
+	    //Location; TODO 
+	    //List<Action> plan; TODO;
+	    public Action nextAction {get; private set;}
+	    //Inventory; // TODO codes/objects/artifacts acquired from the player?
+	    private Dictionary <Character, feelingsAboutChar> Relationships {get; set;}
     }
+	
+	/// <summary>
+	/// Feelings possessed by one char towards another. Values should be -2 to +2. 
+	/// </summary>
+	struct feelingsAboutChar 
+	{
+		int Trust {set; get;} // -2 = never believe, +2 = always believe
+		int Like {set; get;} // -2 = hate, +2 = adore
+	}
 }
